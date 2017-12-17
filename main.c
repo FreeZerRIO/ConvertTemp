4c534be8f66ef3159f0249ea88ca031aea2f8b54
FreeZerRIO <31537782+FreeZerRIO@users.noreply.github.com>
2017-12-17 21:01:01 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,84 +1,76 @@
-//
-//  main.c
-//  Temperature Convert
-//
-//  Created by Алексей Баринов on 14.10.2017.
-//  Copyright © 2017 Era Company. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     float t;
     char system;
     sscanf(argv[1], "%f", &t);
     sscanf(argv[2], "%c", &system);
     switch (system) {
-        case 'C': //Если выбран Цельсий
-            if (t > -273.15)
-        {
-            printf("C - %.2f\n", t);
-            printf("K - %.2f\n", t+273.15);
-            printf("F - %.2f\n", t*1.8 + 32);
-        }
-            else {printf("Вводимая температура ниже абсолютного нуля\n");} //Ошибка нарушения правил физики
-            break;
-        case 'c': //Если выбран Цельсий
-            if (t > -273.15)
-        {
-            printf("C - %.2f\n", t);
-            printf("K - %.2f\n", t+273.15);
-            printf("F - %.2f\n", t*1.8 + 32);
-        }
-            else {printf("Вводимая температура ниже абсолютного нуля\n");}
-            break;
-        case 'K':
-            if (t > 0)
-        {
-            printf("C - %.2f\n", t - 273.15);
-            printf("K - %.2f\n", t);
-            printf("F - %.2f\n", 1.8*(t - 273)+32);
-        }
-            else {printf("Вводимая температура ниже абсолютного нуля\n");}
-            break;
-        case 'k':
-            if (t > 0)
-        {
-            printf("C - %.2f\n", t - 273.15);
-            printf("K - %.2f\n", t);
-            printf("F - %.2f\n", 1.8*(t - 273)+32);
-        }
-            else {printf("Вводимая температура ниже абсолютного нуля");}
-            break;
-        case 'F':
-            if (t > -459.67)
-        {
-            printf("C - %.2f\n", (t-32)*0.55);
-            printf("K - %.2f\n", ((t-32)/1.8)+273.15);
-            printf("F - %.2f\n", t);
-        }
-            else {printf("Вводимая температура ниже абсолютного нуля\n");}
-            break;
-        case 'f':
-            if (t > -459.67)
-        {
-            printf("C - %.2f\n", (t-32)*0.55);
-            printf("K - %.2f\n", ((t-32)/1.8)+273.15);
-            printf("F - %.2f\n", t);
-        }
-            else {printf("Вводимая температура ниже абсолютного нуля");}
-            break;
-        default:
-            printf("Неверный ввод - выбрана с.с. цельсий\n");
+        case 'C': //Celsi do
             if (t > -273.15)
             {
                 printf("C - %.2f\n", t);
                 printf("K - %.2f\n", t+273.15);
                 printf("F - %.2f\n", t*1.8 + 32);
             }
-            else {printf("Вводимая температура ниже абсолютного нуля\n");}
+            else {printf("The input temperature is below absolute zero\n");} //Phisics Error
+            break;
+        case 'c':
+            if (t > -273.15)
+            {
+                printf("C - %.2f\n", t);
+                printf("K - %.2f\n", t+273.15);
+                printf("F - %.2f\n", t*1.8 + 32);
+            }
+            else {printf("The input temperature is below absolute zero\n");}
+            break;
+        case 'K': //Kelvin do...
+            if (t > 0)
+            {
+                printf("C - %.2f\n", t - 273.15);
+                printf("K - %.2f\n", t);
+                printf("F - %.2f\n", 1.8*(t - 273)+32);
+            }
+            else {printf("The input temperature is below absolute zero\n");}
+            break;
+        case 'k':
+            if (t > 0)
+            {
+                printf("C - %.2f\n", t - 273.15);
+                printf("K - %.2f\n", t);
+                printf("F - %.2f\n", 1.8*(t - 273)+32);
+            }
+            else {printf("The input temperature is below absolute zero");}
+            break;
+        case 'F': //Faren do...
+            if (t > -459.67)
+            {
+                printf("C - %.2f\n", (t-32)*0.55);
+                printf("K - %.2f\n", ((t-32)/1.8)+273.15);
+                printf("F - %.2f\n", t);
+            }
+            else {printf("The input temperature is below absolute zero\n");}
+            break;
+        case 'f':
+            if (t > -459.67)
+            {
+                printf("C - %.2f\n", (t-32)*0.55);
+                printf("K - %.2f\n", ((t-32)/1.8)+273.15);
+                printf("F - %.2f\n", t);
+            }
+            else {printf("The input temperature is below absolute zero");} //Input Error
+            break;
+        default:
+            printf("Invalid input - selected number system Celsius\n");
+            if (t > -273.15)
+            {
+                printf("C - %.2f\n", t);
+                printf("K - %.2f\n", t+273.15);
+                printf("F - %.2f\n", t*1.8 + 32);
+            }
+            else {printf("The input temperature is below absolute zero\n");}
             break;
     }
     return 0;
 }
+
